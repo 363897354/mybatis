a4f55c41b73f1d17e7b50bb54669b142931ff55a
xinhua-niujianwei <bj_niujw@aliyun.com>
2020-04-19 20:05:19 +0800

    ddd

--- src/main/java/com/example/mybaits/dao/test.java
+++ src/main/java/com/example/mybaits/dao/test.java
@@ -4,10 +4,9 @@ public class test {
 
     public static void main(String[] args) {
 
-    String  s="a";
-
-
-      int b= 0;
+        String  s="a";
+        System.out.println("第一次提交。。。。。。");
+        int b= 0;
 
     }
 }
